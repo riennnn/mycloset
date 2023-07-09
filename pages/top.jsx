447ea05da45e2d6dfa5cef7396969e7bc030dbdf
rie.n <x1111.rie@gmail.com>
2023-07-09 15:11:09 +0900

    top修正

--- pages/top.jsx
+++ pages/top.jsx
@@ -8,15 +8,23 @@ function Top() {
   const router = useRouter();
   const isLoggedIn = useLoggedIn();
 
+  const TITLE = [
+    {value: "closet", title: "i have ..."},
+    {value: "buy", title: "i want to buy ..."},
+    {value: "recycle", title: "i want to recycle ..."},
+    {value: "createItem", title: "add new item ..."}
+  ]
+
   return (
     <div style={{background:"url(/images/top.jpg)", backgroundSize: "cover", backgroundPosition: "center"}}>
       <Header />
       <main className={styles.main}>
         <div className={styles.container}>
-          <h2 className={styles.title}><Link href='/closet'>i have ...</Link></h2>
-          <h2 className={styles.title}><Link href='buy'>i want to buy ...</Link></h2>
-          <h2 className={styles.title}><Link href='recycle'>i want to recycle ...</Link></h2>
-          <h2 className={styles.title}><Link href='createItem'>add new item ...</Link></h2>
+          {TITLE.map(({value, title}) => (
+            <h2 className={styles.title} key={value}>
+              <Link href={`/${value}`}>{title}</Link>
+            </h2>
+          ))}
         </div>
       </main>
     </div>
